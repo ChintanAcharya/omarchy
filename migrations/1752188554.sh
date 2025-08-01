de2caa5f849454bc9cd4372e5bfc4bfbd3802a51
Chintan <contact@chintanacharya.dev>
2025-08-02 01:51:30 +0530

    Replace Chromium with Google Chrome

--- migrations/1752188554.sh
+++ migrations/1752188554.sh
@@ -1,7 +1,7 @@
-echo "Update chromium.desktop to ensure we are always using wayland"
-if [[ ! -f ~/.local/share/applications/chromium.desktop ]]; then
-  cp ~/.local/share/omarchy/applications/chromium.desktop ~/.local/share/applications/
-  xdg-settings set default-web-browser chromium.desktop
-  xdg-mime default chromium.desktop x-scheme-handler/http
-  xdg-mime default chromium.desktop x-scheme-handler/https
+echo "Update google-chrome.desktop to ensure we are always using wayland"
+if [[ ! -f ~/.local/share/applications/google-chrome.desktop ]]; then
+  cp ~/.local/share/omarchy/applications/google-chrome.desktop ~/.local/share/applications/
+  xdg-settings set default-web-browser google-chrome.desktop
+  xdg-mime default google-chrome.desktop x-scheme-handler/http
+  xdg-mime default google-chrome.desktop x-scheme-handler/https
 fi
