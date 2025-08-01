2b1a0368ab5b7ba1e4e7c47cd48a2f85956a3c0e
Chintan <contact@chintanacharya.dev>
2025-08-02 02:48:39 +0530

    Remove 1password

--- install/apps/xtras.sh
+++ install/apps/xtras.sh
@@ -11,9 +11,6 @@ if [ -z "$OMARCHY_BARE" ]; then
     yay -S --noconfirm --needed "$pkg" ||
       echo -e "\e[31mFailed to install $pkg. Continuing without!\e[0m"
   done
-
-  yay -S --noconfirm --needed 1password-beta 1password-cli ||
-    echo -e "\e[31mFailed to install 1password. Continuing without!\e[0m"
 fi
 
 # Copy over Omarchy applications
