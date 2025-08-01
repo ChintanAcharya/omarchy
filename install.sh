2709a41952d258789e16c8785f197e7ab57371c2
Chintan <contact@chintanacharya.dev>
2025-08-02 01:37:25 +0530

    Add config for increasing file watch limit

--- install.sh
+++ install.sh
@@ -36,6 +36,7 @@ show_subtext "Let's install Omarchy! [1/5]"
 source $OMARCHY_INSTALL/config/identification.sh
 source $OMARCHY_INSTALL/config/config.sh
 source $OMARCHY_INSTALL/config/detect-keyboard-layout.sh
+source $OMARCHY_INSTALL/config/file-watches.sh
 source $OMARCHY_INSTALL/config/fix-fkeys.sh
 source $OMARCHY_INSTALL/config/network.sh
 source $OMARCHY_INSTALL/config/power.sh
