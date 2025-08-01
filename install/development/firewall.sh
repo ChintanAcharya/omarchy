169dd024e3d4da342e035f6f9dd50429a8d901ae
Chintan <contact@chintanacharya.dev>
2025-08-02 01:09:18 +0530

    Do not allow SSH in

--- install/development/firewall.sh
+++ install/development/firewall.sh
@@ -11,9 +11,6 @@ if ! command -v ufw &>/dev/null; then
   sudo ufw allow 53317/udp
   sudo ufw allow 53317/tcp
 
-  # Allow SSH in
-  sudo ufw allow 22/tcp
-
   # Allow Docker containers to use DNS on host
   sudo ufw allow in on docker0 to any port 53
 
