4538bcc436606c477fa3530f3e362c7f0d760e8b
Chintan <contact@chintanacharya.dev>
2025-08-02 00:58:46 +0530

    Remove basecamp

--- install/apps/webapps.sh
+++ install/apps/webapps.sh
@@ -2,7 +2,6 @@
 
 if [ -z "$OMARCHY_BARE" ]; then
   source ~/.local/share/omarchy/default/bash/functions
-  web2app "Basecamp" https://launchpad.37signals.com https://basecamp.com/assets/images/general/basecamp.png
   web2app "WhatsApp" https://web.whatsapp.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/whatsapp.png
   web2app "Google Photos" https://photos.google.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-photos.png
   web2app "Google Contacts" https://contacts.google.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-contacts.png
